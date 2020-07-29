f7d9b7e85fbadd7552058452a1d3b467c5607de4
Eric Bear <ericchristopherbear@gmail.com>
2020-07-29 17:07:44 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+'''
+Code for Albania 2020
+Assignment 1: Tic Tac Toe (Drawing)
+Due: 2 August 2020
+Name: [insert name here]
+
+Overview: Draw a 3x3 grid and add 4 shapes (two stars and two diamonds)
+as though you are playing Tic Tac Toe. See assignment handout for more
+details. As a challenge, you may draw the Albanian Flag using turtle
+and include your code (commented out) below.
+'''
+import turtle
+
+t = turtle.Turtle()
